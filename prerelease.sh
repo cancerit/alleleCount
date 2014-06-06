d199c80b1b35d5c55e53a26bfcf46027ed3ced5b
Keiran Raine <keiranmraine@gmail.com>
2014-06-06 14:30:16 +0100

    fix up prerelease script

--- prerelease.sh
+++ prerelease.sh
@@ -38,14 +38,14 @@ if [ -z "$MY_PATH" ] ; then
   exit 1  # fail
 fi
 # change into the location of the script
-cd $MY_PATH
+cd $MY_PATH/perl
 
 echo '### Running perl tests ###'
 
-export HARNESS_PERL_SWITCHES=-MDevel::Cover=-db,perl/reports,-ignore,'t/.*\.t'
-rm -rf perl/docs
-mkdir -p perl/docs/reports_text
-prove --nocolor -I perl/lib perl/t| sed 's/^/  /' # indent output of prove
+export HARNESS_PERL_SWITCHES=-MDevel::Cover=-db,reports,-ignore,'t/.*\.t'
+rm -rf docs
+mkdir -p docs/reports_text
+prove --nocolor -I lib t | sed 's/^/  /' # indent output of prove
 if [[ $? -ne 0 ]] ; then
   echo "\n\tERROR: TESTS FAILED\n"
   exit 1
@@ -53,19 +53,19 @@ fi
 
 echo '### Generating test/pod coverage reports ###'
 # removed 'condition' from coverage as '||' 'or' doesn't work properly
-cover -coverage branch,subroutine,pod -report_c0 50 -report_c1 85 -report_c2 100 -report html_basic perl/reports -silent > /dev/null
-cover -coverage branch,subroutine,pod -report text perl/reports -silent > perl/docs/reports_text/coverage.txt
-rm -rf perl/reports/structure perl/reports/digests perl/reports/cover.13 perl/reports/runs
-cp perl/reports/coverage.html perl/reports/index.html
-mv perl/reports perl/docs/reports_html
+cover -coverage branch,subroutine,pod -report_c0 50 -report_c1 85 -report_c2 100 -report html_basic reports -silent > /dev/null
+cover -coverage branch,subroutine,pod -report text reports -silent > docs/reports_text/coverage.txt
+rm -rf reports/structure perl.reports/digests reports/cover.13 reports/runs
+cp reports/coverage.html reports/index.html
+mv reports docs/reports_html
 unset HARNESS_PERL_SWITCHES
 
 echo '### Generating POD ###'
-mkdir -p perl/docs/pod_html
-perl -MPod::Simple::HTMLBatch -e 'Pod::Simple::HTMLBatch::go' perl/lib:perl/bin perl/docs/pod_html > /dev/null
+mkdir -p docs/pod_html
+perl -MPod::Simple::HTMLBatch -e 'Pod::Simple::HTMLBatch::go' lib:bin docs/pod_html > /dev/null
 
 echo '### Archiving docs folder ###'
-tar cz -C $INIT_DIR/perl -f perl/docs.tar.gz docs
+tar cz -C $MY_PATH/perl -f docs.tar.gz docs
 
 # generate manifest, and cleanup
 echo '### Generating MANIFEST ###'
