739db2188337299994c988821a60e189cf3d253b
David Jones <drj@sanger.ac.uk>
2014-08-07 16:26:42 +0100

    Tests added for bam_access

--- c/tests/bam_access_tests.c
+++ c/tests/bam_access_tests.c
@@ -20,10 +20,55 @@
 */
 
 #include "minunit.h"
+#include <bam_access.h>
+
+char *test_bam = "../testData/test.bam";
+
+char *test_bam_access_get_position_base_counts(){
+	//Check with default settings
+	char *chr = "22";
+	int pos = 16165776;
+	int chk = -1;
+	chk = bam_access_openbam(test_bam);
+	check(chk == 0,"Error trying to open bam file '%s'.",test_bam);
+	loci_stats *stats = bam_access_get_position_base_counts(chr,pos);
+	mu_assert(stats->base_counts[0]==2,"Check A count 1");
+	mu_assert(stats->base_counts[1]==17,"Check C count 1");
+	mu_assert(stats->base_counts[2]==0,"Check G count 1");
+	mu_assert(stats->base_counts[3]==0,"Check T count 1");
+
+	free(stats->base_counts);
+
+	int min_bq = 15;
+	bam_access_min_base_qual(min_bq);
+	stats = bam_access_get_position_base_counts(chr,pos);
+	mu_assert(stats->base_counts[0]==2,"Check A count 2");
+	mu_assert(stats->base_counts[1]==18,"Check C count 2");
+	mu_assert(stats->base_counts[2]==0,"Check G count 2");
+	mu_assert(stats->base_counts[3]==0,"Check T count 2");
+
+	free(stats->base_counts);
+
+	int min_mq = 15;
+	bam_access_min_map_qual(min_mq);
+	stats = bam_access_get_position_base_counts(chr,pos);
+	mu_assert(stats->base_counts[0]==2,"Check A count 3");
+	mu_assert(stats->base_counts[1]==24,"Check C count 3");
+	mu_assert(stats->base_counts[2]==0,"Check G count 3");
+	mu_assert(stats->base_counts[3]==0,"Check T count 3");
+
+	free(stats->base_counts);
+
+	bam_access_closebam();
+
+	return NULL;
+error:
+	return "1";
+}
 
 char *all_tests() {
    mu_suite_start();
-   //mu_run_test();
+   mu_run_test(test_bam_access_get_position_base_counts);
    return NULL;
 }
 
