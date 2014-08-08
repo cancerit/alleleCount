790b371307b267f006a66c3dbd7e2784dddb957a
David Jones <drjdevelper@users.noreply.github.com>
2014-08-08 15:35:28 +0100

    Updated LICENSE

--- c/tests/minunit.h
+++ c/tests/minunit.h
@@ -3,9 +3,9 @@
 * 
 * Author: Cancer Genome Project cgpit@sanger.ac.uk 
 * 
-* This file is part of CaVEMan. 
+* This file is part of alleleCount. 
 * 
-* CaVEMan is free software: you can redistribute it and/or modify it under 
+* alleleCount is free software: you can redistribute it and/or modify it under 
 * the terms of the GNU Affero General Public License as published by the Free 
 * Software Foundation; either version 3 of the License, or (at your option) any 
 * later version. 
