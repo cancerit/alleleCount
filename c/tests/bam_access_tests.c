47f18b9a7f3f8632d20a3f0a553ce8ac39f98a3e
Matthew Young <my4@sanger.ac.uk>
2018-02-05 12:27:51 +0000

    Added tests of 10X processing along with test data.

--- c/tests/bam_access_tests.c
+++ c/tests/bam_access_tests.c
@@ -24,6 +24,8 @@
 
 char *test_bam = "../testData/test.bam";
 char *test_ref = "../testData/ref.fa.fai";
+char *test_bam10X = "../testData/test10X.bam";
+char *test_10X_cnts = "../testData/test10X_true.cnts";
 
 char *test_bam_access_get_position_base_counts(){
 	//Check with default settings
@@ -73,10 +75,48 @@ char *test_bam_access_get_position_base_counts(){
 	mu_assert(stats->base_counts[1]==24,"Check C count 3");
 	mu_assert(stats->base_counts[2]==0,"Check G count 3");
 	mu_assert(stats->base_counts[3]==0,"Check T count 3");
-
 	free(stats->base_counts);
-	free(stats);
 	bam_access_closehts();
+
+   //Check 10X mode
+   //Open the 10X BAM
+	bam_access_min_base_qual(20);
+	bam_access_min_map_qual(200);
+	bam_access_inc_flag(0);
+	bam_access_exc_flag(0);
+	chk = bam_access_openhts(test_bam10X,test_ref);
+	check(chk == 0,"Error trying to open bam file '%s'.",test_bam10X);
+   FILE *output = fopen("../testData/test10X.cnts","w");
+   int loci_count=1;
+	chr = "1";
+	pos = 198661939;
+	stats->pos = pos;
+	stats->chr = chr;
+	stats->base_counts = malloc(sizeof(int) * 4);
+	stats->base_counts[0] = 0;
+	stats->base_counts[1] = 0;
+	stats->base_counts[2] = 0;
+	stats->base_counts[3] = 0;
+   res = bam_access_get_multi_position_base_counts(&stats,loci_count,1,output);
+   fclose(output);
+	free(stats->base_counts);
+   //Now load the output and check it's correct
+   FILE *gold = fopen(test_10X_cnts,"r");
+   FILE *cnts10X = fopen("../testData/test10X.cnts","r");
+   //Check we can get the files
+   check(cnts10X != NULL && gold !=NULL,"Error trying to open 10X output file.");
+   //Now compare them for being identical
+   int ch1,ch2;
+   do{
+     ch1 = getc(gold);
+     ch2 = getc(cnts10X);
+   }while((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2));
+   mu_assert(ch1==ch2,"Check 10X output");
+   fclose(cnts10X);
+   fclose(gold);
+
+	bam_access_closehts();
+	free(stats);
 
 	return NULL;
 error:
