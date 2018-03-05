7a02471e5471fa014d3ea236a39d608be2e2edaa
Keiran Raine <kr2@sanger.ac.uk>
2018-03-05 11:09:55 +0000

    License dates and email contact details

--- perl/lib/Sanger/CGP/AlleleCount/Genotype.pm
+++ perl/lib/Sanger/CGP/AlleleCount/Genotype.pm
@@ -3,7 +3,7 @@ package Sanger::CGP::AlleleCount::Genotype;
 ##########LICENCE##########
 # Copyright (c) 2014-2018 Genome Research Ltd.
 #
-# Author: CancerIT <cgpit@sanger.ac.uk>
+# Author: CASM/Cancer IT <cgphelp@sanger.ac.uk>
 #
 # This file is part of alleleCount.
 #
