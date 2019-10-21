272d4ddb8a845228f2146ae7e5dca4b9a6b9714a
danchubb <danielchubb@gmail.com>
2019-10-21 12:55:08 +0100

    Update bam_access.c
    make sure that qpos and length are what they think. If they are position on read and length of read, next step is to skip if position <=n or >= length -n
    		printf("Read posis %d\n" , p->qpos);
    		printf("Read posis %d\n" , p->alignment->query->length);

--- c/src/bam_access.c
+++ c/src/bam_access.c
@@ -118,6 +118,9 @@ void pileupCounts(const bam_pileup1_t *pil, int n_plp, loci_stats *stats){
 	for(i=0;i<n_plp;i++){
 		const bam_pileup1_t *p = pil + i;
 		int qual = bam_get_qual(p->b)[p->qpos];
+		printf("Read posis %d\n" , p->qpos);
+		printf("Read posis %d\n" , p->alignment->query->length);
+		int read_pos=p->qpos
 		uint8_t c = bam_seqi(bam_get_seq(p->b), p->qpos);
 		int absent;
     k = kh_put(strh, h, bam_get_qname(p->b), &absent);
