e2d7c4802a2b723e38f97c4f758451a0c9597368
danchubb <danielchubb@gmail.com>
2019-10-21 15:56:13 +0100

    Update alleleCounter.c

--- c/src/alleleCounter.c
+++ c/src/alleleCounter.c
@@ -29,7 +29,7 @@
 
 static int min_base_q = 20;
 static int min_map_q = 35;
-static int end_clip = 0;
+static int end_clip = 10;
 static char *hts_file;
 static char *loci_file;
 static char *out_file;
